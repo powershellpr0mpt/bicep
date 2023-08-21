7a49ca7382e41b6c4e86bc883d03afec8a0d8700
Stephen Weatherford (MSFT) <stephwe@microsoft.com>
2023-08-21 15:03:23 -0700

    spell

--- src/Bicep.Core.UnitTests/Registry/SourceArchiveTests.cs
+++ src/Bicep.Core.UnitTests/Registry/SourceArchiveTests.cs
@@ -67,7 +67,7 @@ public class SourceArchiveTests
     }";
 
     private const string TemplateSpecJsonSource = @"{
-            // Templace spec
+            // Template spec
             ""$schema"": ""https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#"",
             ""contentVersion"": ""1.0.0.0"",
             ""metadata"": {
